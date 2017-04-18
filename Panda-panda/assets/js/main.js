3f2a7e233f62b264cd1b9646013d0cbf49fa7755
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-04-18 13:20:14 -0500

    agregue diseno

--- Panda-panda/assets/js/main.js
+++ Panda-panda/assets/js/main.js
@@ -1,4 +1,5 @@
 var img = document.getElementsByClassName('imagen');
+var cerrar = document.getElementsByClassName("cerrar");
 
 for(i = 0; i<img.length; i++){
   img[i].addEventListener("click", eliminar);
